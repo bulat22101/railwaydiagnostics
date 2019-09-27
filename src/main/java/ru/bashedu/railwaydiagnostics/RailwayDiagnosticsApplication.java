a99ac9672ecdb62d2ed85a8eccd9afe8f5737f6b
Bulat Khabibullin <b-khabibullin@yandex-team.ru>
2019-09-28 01:19:41 +0300

    RD#11

--- src/main/java/ru/bashedu/railwaydiagnostics/RailwayDiagnosticsApplication.java
+++ src/main/java/ru/bashedu/railwaydiagnostics/RailwayDiagnosticsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RailwaydiagnosticsApplication {
+public class RailwayDiagnosticsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RailwaydiagnosticsApplication.class, args);
+        SpringApplication.run(RailwayDiagnosticsApplication.class, args);
     }
 
 }
---
+++ src/main/java/ru/bashedu/railwaydiagnostics/controller/RailwayDiagnosticsController.java
@@ -0,0 +1,38 @@
+package ru.bashedu.railwaydiagnostics.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.bashedu.railwaydiagnostics.user.WorkerDao;
+
+import java.util.Optional;
+
+@RestController
+public class RailwayDiagnosticsController {
+    private final WorkerDao workerDao;
+
+    public RailwayDiagnosticsController(WorkerDao workerDao) {
+        this.workerDao = workerDao;
+    }
+
+    @GetMapping("/leap")
+    public void leap() {
+    }
+
+    @GetMapping("/start")
+    public Double start(@RequestParam Long id, @RequestParam(value = "train") Long trainId, @RequestParam String device) {
+        Optional<Boolean> isInTrip = workerDao.isInTrip(id);
+        if(!isInTrip.isPresent()){
+            throw new IllegalArgumentException("No such worker");
+        }
+        if(isInTrip.get()){
+            throw new IllegalArgumentException("Previous trip is not ended yet");
+        }
+        workerDao.launchTrip(id, trainId, device);
+        return 1.0;
+    }
+
+    @GetMapping("/stop")
+    public void stop() {
+    }
+}
