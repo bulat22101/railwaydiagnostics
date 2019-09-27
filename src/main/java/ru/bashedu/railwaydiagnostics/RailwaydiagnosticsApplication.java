72fa9f98a3c511fd566bc17dbe693d6ecaa023a6
Bulat Khabibullin <b-khabibullin@yandex-team.ru>
2019-09-27 18:20:14 +0300

    Initial commit

---
+++ src/main/java/ru/bashedu/railwaydiagnostics/RailwaydiagnosticsApplication.java
@@ -0,0 +1,13 @@
+package ru.bashedu.railwaydiagnostics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwaydiagnosticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailwaydiagnosticsApplication.class, args);
+    }
+
+}
