b71ba72af0e79237da901f475f380f48cfe282d8
Bulat Khabibullin <b-khabibullin@yandex-team.ru>
2019-09-28 19:50:04 +0300

    RD#19

---
+++ src/main/java/ru/bashedu/railwaydiagnostics/dto/ThresholdDto.java
@@ -0,0 +1,9 @@
+package ru.bashedu.railwaydiagnostics.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data
+@AllArgsConstructor
+public class ThresholdDto {
+    private Double threshold;
+}
